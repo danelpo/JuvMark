88c144c151d792c89b33c92febf1a8d83642e40f
Eric Herscovich <eric.herscovich@connectus.ai>
2020-04-17 12:36:05 -0400

    Added curriculum getter, doesn't save to file yet

--- src/main/java/com/packagename/myapp/RetriveCreateClassData.java
+++ src/main/java/com/packagename/myapp/RetriveCreateClassData.java
@@ -17,4 +17,24 @@ public class RetriveCreateClassData{
         savedname = classcode.concat("_").concat(date);
     }
 
+    public static void getCurriculum(String passedCurriculum){
+        String curriculumNumber;
+        String description;
+
+        //Find underscore 
+        for(int i = 0; i < passedCurriculum.length(); i++){
+            char underscoreFinder = passedCurriculum.charAt(i);
+            int underscoreLocation = i;
+
+            if(underscoreFinder == '_'){
+                underscoreLocation = i;
+                curriculumNumber = passedCurriculum.substring(0, underscoreLocation);
+                description = passedCurriculum.substring(underscoreLocation+1);
+                System.out.println(curriculumNumber);
+                System.out.println(description);
+            }
+        }
+
+    }
+
 }
