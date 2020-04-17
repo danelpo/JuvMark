d4ed8a4724f963c1e0e230ec0927c64a26e4a3a9
Eric Herscovich <eric.herscovich@connectus.ai>
2020-04-17 15:03:04 -0400

    CSV will now be made using class code and dates/sem, passed curriculum data will be sent into CSV.

--- src/main/java/com/packagename/myapp/RetriveCreateClassData.java
+++ src/main/java/com/packagename/myapp/RetriveCreateClassData.java
@@ -1,9 +1,15 @@
 package com.packagename.myapp;
 
-public class RetriveCreateClassData{
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class RetriveCreateClassData {
     static String classcode;
     static String date;
     static String savedname;
+    static String csvPath;
 
     public static void setClasscode(String passedClasscode) {
         classcode = passedClasscode;
@@ -15,12 +21,13 @@ public class RetriveCreateClassData{
 
     public static void setSavedname() {
         savedname = classcode.concat("_").concat(date);
+        csvPath = "Data/Curriculums/" + savedname + ".csv";
     }
 
-    public static void getCurriculum(String passedCurriculum){
+    public static void getCurriculum(String passedCurriculum) {
         String curriculumNumber;
         String description;
-
+        System.out.println("test");
         //Find underscore 
         for(int i = 0; i < passedCurriculum.length(); i++){
             char underscoreFinder = passedCurriculum.charAt(i);
@@ -30,11 +37,24 @@ public class RetriveCreateClassData{
                 underscoreLocation = i;
                 curriculumNumber = passedCurriculum.substring(0, underscoreLocation);
                 description = passedCurriculum.substring(underscoreLocation+1);
-                System.out.println(curriculumNumber);
-                System.out.println(description);
+                
+                try {
+                    FileWriter fw = new FileWriter(csvPath, true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    PrintWriter pw = new PrintWriter(bw);
+        
+                    pw.println(curriculumNumber + "," + description);
+                    pw.flush();
+                    pw.close();
+
+                    System.out.println("Record saved");
+                } 
+                catch (IOException e) {
+                    System.out.println("Record not saved");
+                }
             }
         }
-
+        
     }
 
 }
---
+++ src/main/java/com/packagename/myapp/TestingClass.java
@@ -0,0 +1,13 @@
+package com.packagename.myapp;
+
+public class TestingClass {
+    
+    public static void Test(){
+        RetriveCreateClassData.setClasscode("ICS4U");
+        RetriveCreateClassData.setDate("2019-2020_2");
+        RetriveCreateClassData.setSavedname();
+        RetriveCreateClassData.getCurriculum("B4_123456789");
+        RetriveCreateClassData.getCurriculum("A2_123456789");
+    }
+   
+}
