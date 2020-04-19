2c65f9c7fdc8bea8dabf3a0bd1da7de4a5929af8
picuron <eric.herscovich@gmail.com>
2020-04-19 15:10:53 -0400

    Setting up JSON for tasks

--- src/main/java/com/myapp/juvmark/CreateClassDataSorting.java
+++ src/main/java/com/myapp/juvmark/CreateClassDataSorting.java
@@ -4,9 +4,12 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.file.Files;
 
-//import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
 
 public class CreateClassDataSorting {
     static String classcode;
@@ -15,11 +18,13 @@ public class CreateClassDataSorting {
     static String csvCurriculumPath;
     static String JSONTaskPath;
 
-    public static void setClasscode(String passedClasscode) {
+    static Gson taskJSON = new Gson();
+
+    public static void setClasscode(final String passedClasscode) {
         classcode = passedClasscode;
     }
 
-    public static void setDate(String passedDate) {
+    public static void setDate(final String passedDate) {
         date = passedDate;
     }
 
@@ -29,42 +34,41 @@ public class CreateClassDataSorting {
         JSONTaskPath = "Data/Tasks/" + savedname + "tasks.json";
     }
 
-    public static void getCurriculum(String passedCurriculum) {
+    public static void getCurriculum(final String passedCurriculum) {
         String curriculumNumber;
         String description;
-        
-        //Find underscore 
-        for(int i = 0; i < passedCurriculum.length(); i++){
-            char underscoreFinder = passedCurriculum.charAt(i);
+
+        // Find underscore
+        for (int i = 0; i < passedCurriculum.length(); i++) {
+            final char underscoreFinder = passedCurriculum.charAt(i);
             int underscoreLocation = i;
 
-            if(underscoreFinder == '_'){
+            if (underscoreFinder == '_') {
                 underscoreLocation = i;
                 curriculumNumber = passedCurriculum.substring(0, underscoreLocation);
-                description = passedCurriculum.substring(underscoreLocation+1);
-                
-                //Creates csv file, then writes to it.
+                description = passedCurriculum.substring(underscoreLocation + 1);
+
+                // Creates csv file, then writes to it.
                 try {
-                    FileWriter fw = new FileWriter(csvCurriculumPath, true);
-                    BufferedWriter bw = new BufferedWriter(fw);
-                    PrintWriter pw = new PrintWriter(bw);
-        
+                    final FileWriter fw = new FileWriter(csvCurriculumPath, true);
+                    final BufferedWriter bw = new BufferedWriter(fw);
+                    final PrintWriter pw = new PrintWriter(bw);
+
                     pw.println(curriculumNumber + "," + description + "_curriculum");
                     pw.flush();
                     pw.close();
 
                     System.out.println("Successfully interacted with CSV");
-                } 
-                catch (IOException e) {
+                } catch (final IOException e) {
                     System.out.println("Error with CSV");
                 }
             }
         }
-        
+
     }
 
-    //Accepts passed in tasks
-    public static void getTasks(String passedTasks) {
+    // Accepts passed in tasks
+    public static void getTasks(final String passedTasks) {
         String tasknumber = "null";
         String curriculumNumbers = "null";
         String[] curriculumNumbersArray;
@@ -76,71 +80,84 @@ public class CreateClassDataSorting {
         int underscoreLocation2 = 0;
         int arraySize = 0;
 
-        //Creates JSON file for tasks
-        try {
-            FileWriter file = new FileWriter(JSONTaskPath);
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Find underscores
+        for (int i = 0; i < passedTasks.length(); i++) {
+            final char underscoreFinder = passedTasks.charAt(i);
 
-        //Find underscores
-        for(int i = 0; i < passedTasks.length(); i++){
-            char underscoreFinder = passedTasks.charAt(i);
-
-            //First underscore (1/2)
-            if(underscoreFinder == '_' && numberUnderscores == 0){
+            // First underscore (1/2)
+            if (underscoreFinder == '_' && numberUnderscores == 0) {
                 numberUnderscores++;
                 underscoreLocation1 = i;
                 tasknumber = passedTasks.substring(0, underscoreLocation1);
             }
 
-            //Second underscore (2/2)
-            if(underscoreFinder == '_' && numberUnderscores == 1 && i != underscoreLocation1){
+            // Second underscore (2/2)
+            if (underscoreFinder == '_' && numberUnderscores == 1 && i != underscoreLocation1) {
                 underscoreLocation2 = i;
                 curriculumNumbers = passedTasks.substring(underscoreLocation1 + 1, underscoreLocation2);
                 description = passedTasks.substring(underscoreLocation2 + 1);
-                 
-                //Counts # of curriculums
-                for(int j = 0; j < curriculumNumbers.length(); j++){
-                    char dashFinder = curriculumNumbers.charAt(j);
 
-                    if(dashFinder == '-'){
+                // Counts # of curriculums
+                for (int j = 0; j < curriculumNumbers.length(); j++) {
+                    final char dashFinder = curriculumNumbers.charAt(j);
+
+                    if (dashFinder == '-') {
                         numberDashes++;
                     }
                 }
-                //Initialize curriculum number array;
+                // Initialize curriculum number array;
                 arraySize = numberDashes + 1;
                 curriculumNumbersArray = new String[arraySize];
                 int temp = 0;
-                
-                //Sorts curriculum numbers into array
-                for(int j = 0; j < curriculumNumbers.length(); j++){
-                    char dashFinder = curriculumNumbers.charAt(j); 
 
-                    //For first curriculum number
-                    if(dashFinder == '-' && numberDashesFound == 0){
+                // Sorts curriculum numbers into array
+                for (int j = 0; j < curriculumNumbers.length(); j++) {
+                    final char dashFinder = curriculumNumbers.charAt(j);
+
+                    // For first curriculum number
+                    if (dashFinder == '-' && numberDashesFound == 0) {
                         numberDashesFound++;
                         temp = j;
-                        curriculumNumbersArray[numberDashesFound-1] = curriculumNumbers.substring(0, j);
+                        curriculumNumbersArray[numberDashesFound - 1] = curriculumNumbers.substring(0, j);
                     }
 
-                    //For all curriculum numbers except first and last
-                    if(dashFinder == '-' && temp != j){
+                    // For all curriculum numbers except first and last
+                    if (dashFinder == '-' && temp != j) {
                         numberDashesFound++;
-                        curriculumNumbersArray[numberDashesFound-1] = curriculumNumbers.substring(temp + 1, j);
+                        curriculumNumbersArray[numberDashesFound - 1] = curriculumNumbers.substring(temp + 1, j);
                         temp = j;
                     }
-                    
-                    //For last curriculum number
-                    if(j + 1 == curriculumNumbers.length()){
+
+                    // For last curriculum number
+                    if (j + 1 == curriculumNumbers.length()) {
                         numberDashesFound++;
-                        curriculumNumbersArray[numberDashesFound-1] = curriculumNumbers.substring(temp + 1, curriculumNumbers.length());
+                        curriculumNumbersArray[numberDashesFound - 1] = curriculumNumbers.substring(temp + 1,
+                                curriculumNumbers.length());
                     }
-                } 
-                System.out.println(curriculumNumbersArray[]);
+                }
                 System.out.println(description);
             }
         }
+        System.out.println("Here");
+
+        JsonObject taskDetails = new JsonObject();
+        taskDetails.addProperty("Task Number", tasknumber);
+        taskDetails.addProperty("Curriculums", curriculumNumbers);
+        taskDetails.addProperty("Description", description);
+
+        JsonObject taskObject = new JsonObject();
+        taskObject.add(tasknumber, taskDetails);
+
+        JsonObject taskList = new JsonObject();
+        taskList.add("Tasks", taskObject);
+        System.out.println(taskList.toString());
+
+        try {
+            Writer writer = new FileWriter(JSONTaskPath);
+            taskJSON.toJson(taskList, writer);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
     }
 }
